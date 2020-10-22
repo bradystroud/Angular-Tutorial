6400e8b96d7727ce0421e5281e30e22ddb9e5cd7
Brady Stroud <bradystroud@me.com>
2020-10-22 22:08:57 +1000

    added services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,24 +1,45 @@
-import { HEROES } from './../mock-heroes';
+import { HeroService } from '../hero.service'; // This is the service that provides the Hero list *1
 import { Component, OnInit } from '@angular/core';
-// import { Console } from 'console';
-import { Hero } from '../hero';
+import { Hero } from '../hero'; // This is the hero class.
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
-  selectedHero: Hero;
+  heroes: Hero[]; // *1 Heroes list (empty here. This is the intialisation Syntax).
+  selectedHero: Hero; // This becomes the hero that the user chooses to get more details on.
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
 
-  constructor() { }
+  // The parameter simultaneously defines a private heroService property and identifies it as a HeroService injection site.
+  constructor(private heroService: HeroService, private messageService: MessageService) {}
+  // This was a question i just had and then the tutorial answered it.
+  // It's like the writer actually took the time to consider what readers might not understan when writing the docs. Crazy.
+  //
+  // While you could call getHeroes() in the constructor, that's not the best practice.
+  // Reserve the constructor for simple initialization such as wiring constructor parameters to properties.
+  // The constructor shouldn't do anything.
+  // It certainly shouldn't call a function that makes HTTP requests to a remote server as a real data service would.
+  // Instead, call getHeroes() inside the ngOnInit lifecycle hook and
+  // let Angular call ngOnInit() at an appropriate time after constructing a HeroesComponent instance.
+  //
 
   ngOnInit(): void {
     console.log('Hello, World?');
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService
+      .getHeroes()
+      .subscribe((heroes) => (this.heroes = heroes));
+    // since its asyncrous now, i think the subscribe() waits for the reply
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 }
